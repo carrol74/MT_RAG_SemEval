b9fac4d259e2d4ccbe0cb4fcb08d9e81d6fbd89e
Carol <carrol19741@gmail.com>
2026-01-05 15:49:04 +0100

    move implement

--- src/retriever.py
+++ src/retriever.py
@@ -21,17 +21,13 @@ class MTHybridRetriever:
     #TODO: not explicitly pass documents, we should initialize them inside the model
 
     def index_documents(self, documents, isUpdate=False):
-        self.build_vector_retriever(documents, isUpdate=isUpdate)
-        self.build_bm25_retriever(documents)
+        ...
 
     def build_vector_retriever(self, documents, isUpdate=False):
-        if not isUpdate and os.path.exists(os.path.join(DATA_ROOT, DATA_VECTOR_ROOT, f"{self.domain}")):
-            self.vector_store = MultiDomainVectorStore.load_existing(collection_name=self.domain)
-        else:
-            self.vector_store = MultiDomainVectorStore.build_from_documents(documents)
+        ...
 
     def build_bm25_retriever(self, documents):
-        self.bm25_retriever = BM25Retriever.from_documents(documents)
+        ...
 
     def search(self, query, k=2):
         """
@@ -40,10 +36,4 @@ class MTHybridRetriever:
         Args:
             alpha: Weighting factor between vector store and BM25
         """
-        if self.vector_store is None or self.bm25_retriever is None:
-            raise ValueError("Both vector store and BM25 retriever must be initialized")
-        vector_result = self.vector_store.similarity_search(query, k=k)
-        self.bm25_retriever.k = k
-        bm25_result = self.bm25_retriever.invoke(query)
-        combined_result = []
-        # TODO: Implement weighted combination logic based on alpha
+        ...
